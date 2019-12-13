acd970be70c788c2644f181233a9d53ef48af46f
tautvydascoding <123@gmail.com>
2019-12-13 15:42:46 +0200

    tuscias projektai su js

---
+++ dirbame_cia/diena_5/destytojas/1-js-susipazinimas/js/main.js
@@ -0,0 +1,57 @@
+// test
+console.log("i console labas");
+// alert("issokanti zinute");
+document.write("labas ekrane");
+
+
+
+// ctrl + /    viena eilute
+/*
+  komenratas
+  komenratas
+  komenratas
+  komenratas
+ */
+
+
+var w = 10;   // globalus kintamojo sukurimas
+
+var w = 15;   // viskas gerai!
+w = 3.14 * 10 / 100;
+
+let z = 99;  // globalus kintamojo sukurimas
+// let z = 11;  // ERROR! : negalima kurit tokio pacio kintma
+
+z = 110;
+z = 100 + 888;
+
+const prekiuKiekis = 10; // konstantos - nekinta
+
+// KINTAMUJU TIPAI
+// konstantos - kintamieji kurie nekinta
+
+let vardas = 'Petras'; // String - tekstai
+let amzius = 0;      // Number: integer - sveiki sk.
+let ugis = 188.65;   // Number: float/double - sveiki sk.
+let mirtiesData = null;  //
+let arVedes = true;  // false  BOOLEAN
+let vakariene;     //undefined - kintamasis neturi nustatytos reiksmes
+
+console.log( vakariene );
+// array, objektai
+
+let a = 100;
+let b = 200;
+let c = 1;
+let k;
+k = c - (a * b);
+k = Math.PI *  Math.pow(a, 2);
+console.log( "didelis kugis: " , k );
+console.log("mama ejo", "i parduotuve ", " pirkti");
+
+
+
+
+
+
+//
