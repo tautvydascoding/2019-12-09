14cf96bd274e28fd4acfba5dd5d67aa0d123c620
Tautvydas <tautvydascoding@gmail.com>
2019-12-15 22:17:46 +0200

    6-10days

---
+++ uzduotys/diena_8/TEORIJA-/js-scopes-6-atvejai.js
@@ -0,0 +1,99 @@
+
+// Ka atspausdina console?
+var x = 10;
+fucntion test1() {
+    console.log(x);
+}
+test1();
+
+// Ka atspausdina console?
+var x = 10;
+fucntion test1() {
+    console.log(x);
+}
+
+// Ka atspausdina console?
+var x = 5;
+fucntion test1() {
+    x = 0;
+}
+test1();
+console.log(x);
+
+
+// Ka atspausdina console?
+var x = 5;
+fucntion test1() {
+    x = 0;
+}
+console.log(x);
+test1();
+
+
+// Ka atspausdina console?
+var x = 0;
+fucntion test1() {
+    var x = 100;
+}
+console.log(x);
+test1();
+
+
+// Ka atspausdina console?
+var x = 200;
+fucntion test1(x) {
+    var x = 100;
+    console.log(x);
+}
+test1(555);
+
+// Ka atspausdina console?
+var x = 50;
+fucntion test1(x) {
+    var x = 10;
+}
+test1(99);
+console.log(x);
+
+
+// Ka atspausdina console?
+var x = 22;
+fucntion test1(x) {
+     x = 11;
+}
+test1(13);
+console.log(x);
+
+
+// Ka atspausdina console?
+var x = 33;
+fucntion test1(x) {
+     x = 44;
+     console.log(x);
+}
+test1(55);
+
+
+// Ka atspausdina console?
+var x = 999;
+fucntion test1(x) {
+     console.log(x);
+}
+test1(888);
+
+// Ka atspausdina console?
+var x = 999;
+fucntion test1(x) {
+     console.log(x);
+     x = 555;
+}
+test1(888);
+
+
+// Ka atspausdina console?
+var x = 999;
+fucntion test1(x) {
+     console.log(x);
+     x = 555;
+}
+test1(888);
