a4a6f06a29d0a6f49f4f33ff1bd6a8d3f46da0a6
d-o-m-a-s <domas.zitkevicius@gmail.com>
2019-12-17 12:26:20 +0200

    sub

---
+++ dirbame_cia/diena_6/domas/antra_uzduotis/js/main.js
@@ -0,0 +1,35 @@
+// test
+console.log("labas");
+// alert("issokanti zinute");
+document.write("labas ekrane");
+
+// UZDUOTIS - f-jos return
+failo virusje sukurti kintamuosius:
+valandinisIkainis = 10
+valandos Povilas = 160
+valandos Antanas = 80
+valandos Tomas   = 100
+imones pajamos = 20000
+kitos imones islaidos = 3400
+ UZDUOTIS:
+ 1) sukurti  f-ja: getDarbuotojoAtlyginimas
+ 1.2) paleisti tiek kartu kiek turime darbuotoju
+ 2) sukurti  f-ja: getImonesPelnas
+ 2.2) isvesti apskaiciuota pelna i console ar ekrana
+
+let valandinisIkainis = 10;
+
+let valandosPovilo = 160;
+let valandosAntano = 80;
+let valandosTomas = 100;
+let imonesPajamos = 20000;
+let kitosImonesIslaidos = 3400;
+
+function getDarbuotojoAtlyginimas()
+{
+
+
+
+
+
+}
