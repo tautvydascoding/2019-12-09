5f4af284200981d71593267484a56b80b8fdfd25
tautvydascoding <123@gmail.com>
2019-12-17 12:35:04 +0200

    klases darbai

--- dirbame_cia/diena_7/destytojas/1-js-IF/js/main.js
+++ dirbame_cia/diena_7/destytojas/1-js-IF/js/main.js
@@ -1,3 +1,9 @@
+// test
+console.log("labas");
+// alert("issokanti zinute");
+document.write("labas ekrane");
+
+
 // =================if  teorija====================
 // if ( true ) {
 //     console.log("labas");
@@ -38,6 +44,16 @@ if(true){
 // sukurti kintamuosius: arVedes ir arDuotiPaskola = false
 // patikrinti ar zmogus vedes. Jeigu "vedes" - duoti paskola, jeigu "nevedes" - neduoti paskolos
 // su console.log pranesti ar paskola zmogui bus duodama
+let arVedes = false;
+let arDuotiPaskola = false;
+
+if ( arVedes == true) {
+    arDuotiPaskola = true;
+} else {
+    arDuotiPaskola = false;
+}
+console.log("Ar paskola gausite: " + arDuotiPaskola);
+// ----------------------------------
 
 // 0 UZDUOTIS
 // A)
@@ -45,29 +61,57 @@ if(true){
 // patikrinti:
 // jeigu vardai sutampa, i konsole pranesti apie tai
 // jeigu vardai nesutampa - parasysi "vardai yra skirtingi"
-
+let vardas1 = "Tomas";
+let vardas2 = "Antanas";
+if (vardas1 == vardas2) {
+    console.log(vardas1, vardas2, " - vardai vienodi");
+} else {
+    console.log(vardas1, vardas2, " - vardai yra skirtingi");
+}
 // B)
 // Jeigu vardas1 yra "Tomas" - pasisveikinti su juo
-
+if (vardas1 == "Tomas") {
+    console.log(vardas1 + " - vardas vienas yra Tomas");
+}
 // C)
 // Patikrinti ar varda1 yra "Tomas", IR butinai vardas2  yra "Karolis"
 // pranesti kad vardai: "Tomas ir Karolis"
 // arba pranesti, kad : "vardai NE Tomas ir Karolis"
 // pakeitineti vardus ir paziureti ar veikia
-
+vardas1 = "Tomas";
+vardas2 = "Karolis";
+if (vardas1 == "Tomas" && vardas2 == "Karolis") {
+    console.log(vardas1 , vardas2, " - vardai yra  Tomas ir Karolis ");
+} else {
+    console.log(vardas1 , vardas2, " -!!! vardai NE Tomas ir Karolis ");
+}
 // D)
 // Patikrinti ar bent vienas is vardu "Tomas"
 // || - arba
 // pranesti: "kazkuris is vardu yra Tomas"
 // pakeitineti vardus ir paziureti ar veikia
-
+vardas1 = "Antanas";
+vardas2 = "Karolis";
+if (vardas1 == "Tomas" || vardas2 == "Tomas") {
+    console.log(vardas1 , vardas2, " - kazkuris vardas yra Tomas ");
+} else {
+    console.log(vardas1 , vardas2, " -!!! NEi vienas is vardu Tomas ");
+}
 // 1 UZDUOTIS
 // turesime vartotojo amziu
-// let age = 24;
+let age = 24;
 // gapal ji, turesiem isvesti tam tikra reklamos teksta
 
-
 // Salygos:
+if (age < 7) {
+    console.log("Pliusines varles");
+} else if ( age < 14) {
+    console.log("Mini telefonai");
+} else if (age < 18) {
+    console.log("New music app");
+}
+// let m = [];
+// m[ m.length ] = "tekstas gale";
 
 // iki 7 metu
 //      "Pliusines varles"
@@ -89,19 +133,8 @@ if(true){
 // 2 UZDUOTIS
 // susikurti kintamaji 'klietoVardas' - (ja ivesime betkokia) kurio reiksme lyginsime
 // sukurti 3  vardas1= "Tomas", vardas2 ="Paulius", vardas3 ="Airidas" (gimianiciu vardai , jie tures akcijas))
-let klietoVardas = "Jurate";
-vardas1= "Tomas";
-vardas2 ="Paulius";
-vardas3 ="Airidas";
-// kai "if"-ui padauodame klietoVardas ir vardas1 - isvesti "Masinoms 10% nuolaida"
-if (klietoVardas == vardas1) {
-	console.log("Masinoms 10% nuolaida");
-} else if (klietoVardas == vardas2) {
-	console.log("Buitinei technikai  30% nuolaida");
-} else {
-	console.log("5% nuolaida kelionems");
 
-}
+// kai "if"-ui padauodame klietoVardas ir vardas1 - isvesti "Masinoms 10% nuolaida"
 // kai "if"-ui padauodame klietoVardas ir vardas2 - isvesti "Buitinei technikai  30% nuolaida"
 // kai "if"-ui padauodame bet ka kita - isvesti "5% nuolaida kelionems"
 
