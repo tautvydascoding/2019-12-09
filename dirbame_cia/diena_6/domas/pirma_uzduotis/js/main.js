0cf3a729ac5b926d4a97cfda169dee81681b8d53
d-o-m-a-s <domas.zitkevicius@gmail.com>
2019-12-17 16:43:02 +0200

    submit

--- dirbame_cia/diena_6/domas/pirma_uzduotis/js/main.js
+++ dirbame_cia/diena_6/domas/pirma_uzduotis/js/main.js
@@ -42,10 +42,11 @@ function printKaina(x){
 
 console.log(x.toFixed(2));
 
+document.querySelector("body").innerHTML += x + "<br>";
 
 
 }
-printKaina(15.9999999999999999);
+printKaina(15.99);
 
 
 
@@ -54,14 +55,62 @@ printKaina(15.9999999999999999);
 // iskviesti f-ja print 3 kartus, kad atspausdintu 3 skirtingas kainas:  999 , 13.49, 100.05
 //    vietoj x irasant, koki nors teksta pvz: printKaina( 999);
 
+
+printKaina(4.99);
+printKaina(5.99);
+printKaina(6.99);
+
 // 2.1 UZDUOTIS
 // parasyti f-ja "pazymiuVidurkis()", kuri apskaiciuoja vidurki is  5 menesiu pazymiu
 // suskaiciuoti pazymiu vidurki (pazymius sukurti f-jos viduje) 5, 10 , 8 , 6 , 8  (ir atspausdintu vidurki)
+
+function pazymiuVidurkis(){
+let pazymys1 = 5;
+let pazymys2;
+let pazymys3;
+let pazymys4;
+let pazymys5;
+let vidurkis;
+
+pazymys1 = 5;
+pazymys2 = 10;
+pazymys3 = 8;
+pazymys4 = 6;
+pazymys5 = 8;
+vidurkis = (pazymys1+pazymys2+pazymys3+pazymys4+pazymys5)/5;
+
+
+
+console.log(vidurkis);
+
+
+
+
+}
+pazymiuVidurkis();
+
+
+
 
 // 2.2 UZDUOTIS
 // parasyti f-ja "pazymiuVidurkis1(x1, x2, x3, x4, x5)", kuriai galima paduoti 5 kintamuosius,
 // kuri apskaiciuoja vidurki is paduotu 5 menesiu pazymiu
 // suskaiciuoti pazymiu vidurki, 5, 10 , 8 , 6 , 8  (ir atspausdintu vidurki)
+
+function pazymiuVidurkis22(x1,x2,x3,x4,x5){
+
+vidurkis = (x1+x2+x3+x4+x5)/5;
+
+document.querySelector("body").innerHTML += "<div>"+vidurkis+"</div>";
+
+
+
+
+}
+pazymiuVidurkis22(5,6,8,6,9);
+
+
+
 
 
 //=====================RETURN==================
@@ -70,11 +119,50 @@ printKaina(15.9999999999999999);
 // A) parasyti f-ja "getVardas()", kuri turi "return" zodeli ir grazina varda i iskveitimo vieta.
 // B)   f-joje  sukurti kintamaji "vardas" Tomas
 // C) patikrinti ar veikia f-ja
+function getVardas() {
+
+let vardas = "Tomas";
+
+
+
+
+
+//document.querySelector("body").innerHTML += "<div>vardas()</div>;
+return vardas;
+}
+
+console.log(getVardas ());
+document.querySelector("body").innerHTML += "<div>"+getVardas()+"</div>";
+
+
+
+
+
+
+
 
 // 2 UZDUOTIS
 // parasyti f-ja "getVardasPavarde()", kuri turi "return" zodeli ir grazina varda ir pavarde i iskveitimo vieta.
 //  f-joje sukurti kintamaji   "pavarde" Tomauskas, "vardas" - Antanas
 // patikrinti ar veikia f-ja
+
+function getVardasPavarde(){
+
+let vardas1;
+let pavarde1;
+
+vardas1 = "Antanas";
+pavarde1 = "Tomauskas";
+
+return vardas1 + " " + pavarde1;
+}
+
+document.querySelector("body").innerHTML += "<div>"+getVardasPavarde()+"</div>";
+
+
+
+
+
 
 // 3 UZDUOTIS
 // A) parasyti f-ja "getPelnas()", kuri turi "return" zodeli ir grazina apskaiciuota pelna
@@ -83,16 +171,59 @@ printKaina(15.9999999999999999);
 // B) f-joje apskaiciuoti pelna  ( pvz: pelnas = pajamos - islaidos)
 // C) patikrinti ar veikia f-ja
 
+function getPelnas (pajamos1, islaidos1){
+
+let pajamos = pajamos1;
+let islaidos = islaidos1;
+let pelnas;
+
+pelnas = pajamos - islaidos;
+
+return pelnas;
+
+}
+
+document.querySelector("body").innerHTML += getPelnas(1800, 600) + "</br>";
+
+
 // 3.2 UZDUOTIS
 // A) parasyti f-ja "getPelnas(pajamos, nuostoliai)", kuri turi "return" zodeli ir grazina apskaiciuota pelna
 // B) f-joje apskaiciuoti pelna  ( pvz: pelnas = pajamos - nuostoliai)
 // C) patikrinti ar veikia f-ja
+
+function getPelnas(pajamos, nuostoliai){
+
+let pajamos1;
+let nustoliai1;
+let pelnas;
+pelnas = pajamos - nuostoliai;
+
+return pelnas;
+}
+
+document.querySelector("body").innerHTML += getPelnas(2000, 300) + "</br>";
 
 
 // 4.1 UZDUOTIS
 // parasyti f-ja, kuri skaiciuoja vieno mokinio pazymiu vidurki
 // "getPazymiuVidurkis2(x1, x2, x3, x4, x5)"  f-ja apskaiciuoja vidurki is paduotu 5 menesiu pazymiu (ir grazina atsakyma i iskvietimo vieta! "return atskymas;")
 //  turi "return" zodeli.
+
+function getPazymiuVidurkis2(x1, x2, x3, x4, x5){
+
+let pazymys = x1;
+let pazymys2 = x2;
+let pazymys3 = x3;
+let pazymys4 = x4;
+let pazymys5 = x5;
+
+
+return vidurkis;
+}
+
+document.querySelector("body").innerHTML += getPazymiuVidurkis2(5,8,6,9,7) + "</br>";
+
+
 
 // 4.2 UZDUOTIS
 // parasyti f-ja, kuri skaiciuoja 6-kiu mokiniu pazymiu vidurki
@@ -117,6 +248,45 @@ printKaina(15.9999999999999999);
 // ####
 // #####
 // ######
+
+function zvaigzdutes(){
+let x = "";
+for (var i = 0; i < 6; i++) {
+
+x += "*";
+document.querySelector("body").innerHTML += x + "</br>";
+console.log(x);
+}
+}
+
+zvaigzdutes();
+
+
+
+
+
+
+//failo virusje sukurti kintamuosius:
+//valandinisIkainis = 10
+//valandos Povilas = 160
+//valandos Antanas = 80
+//valandos Tomas   = 100
+//imones pajamos = 20000kitos imones islaidos = 3400
+ //UZDUOTIS:
+ //1) sukurti  f-ja: getDarbuotojoAtlyginimas
+ //1.2) paleisti tiek kartu kiek turime darbuotoju
+ //2) sukurti  f-ja: getImonesPelnas
+ //2.2) isvesti apskaiciuota pelna i console ar ekrana
+
+
+
+
+
+
+
+
+
+
 
 //=========== =KOMANDINE (3-4h) Bootstrap && GIT=================
 // ant lentos
