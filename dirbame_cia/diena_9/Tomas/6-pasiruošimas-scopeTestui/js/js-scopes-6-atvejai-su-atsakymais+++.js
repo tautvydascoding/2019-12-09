82cc9990ae07f0b8a7fff70ca6f3e4e23923f342
tomassakalauskas <tomassakalauskas950510@gmail.com>
2019-12-19 16:47:03 +0200

    ikeliu ketvirtadienio darbus

--- dirbame_cia/diena_9/Tomas/6-pasiruošimas-scopeTestui/js/js-scopes-6-atvejai-su-atsakymais+++.js
+++ dirbame_cia/diena_9/Tomas/6-pasiruošimas-scopeTestui/js/js-scopes-6-atvejai-su-atsakymais+++.js
@@ -1,114 +1,112 @@
 
-// Ka atspausdina console?
+// 1. Ka atspausdina console?
 var x = 10;
 function test1() {
     console.log(x);
 }
-test1(); //10
+test1(); //  10
 
-// Ka atspausdina console?
+// 2. Ka atspausdina console?
 var x = 10;
 function test1() {
     console.log(x);
 }
-// nieko
+// nieko, nes neissaukta
 
-// Ka atspausdina console?
+// 3. Ka atspausdina console?
 var x = 5;
 function test1() {
     x = 0;
 }
 test1();
-console.log(x);
+console.log(x); // 0
 
 
-// Ka atspausdina console?
+// 4. Ka atspausdina console?
 var x = 5;
 function test1() {
     x = 0;
 }
-console.log(x);
+console.log(x);   // 5
 test1();
 
 
-// Ka atspausdina console?
+// 5. Ka atspausdina console?
 var x = 0;
 function test1() {
     var x = 100;
 }
-console.log(x);
+console.log(x); //0
 test1();
 
 
-// Ka atspausdina console?
+// 6. Ka atspausdina console?
 var x = 200;
 function test1(x) {
     var x = 100;
     console.log(x);
 }
-test1(555); //100
+test1(555); // 100
 
-// Ka atspausdina console?
+// 7. Ka atspausdina console?
 var x = 50;
 function test1(x) {
     var x = 10;
 }
 test1(99);
-console.log(x); //50
+console.log(x); // 50
 
 
-// Ka atspausdina console?
+// 8. Ka atspausdina console?
 var x = 22;
 function test1(x) {
      x = 11;
 }
 test1(13);
-console.log(x); //22
+console.log(x); // 22
 
 
-// Ka atspausdina console?
+// 9. Ka atspausdina console?
 var x = 33;
 function test1(x) {
      x = 44;
-     console.log(x);
+     console.log(x); // 44
 }
 test1(55);
 
 
-// Ka atspausdina console?
+// 10. Ka atspausdina console?
 var x = 999;
 function test1(x) {
      console.log(x);
 }
-test1(888);
+test1(888);  // 888
 
-// Ka atspausdina console?
+// 11. Ka atspausdina console?
 var x = 999;
 function test1(x) {
-     console.log(x);
+     console.log(x); // 888
      x = 555;
 }
-test1(888);
-console.log( x );
+test1(888); //
 
-// Ka atspausdina console?
 
+// 12. Ka atspausdina console?
+var x = 999;
+function test1(x) {
+     x = 555;
+     console.log( this.x);
+}
+test1(888); //999
+
+// 13. Ka atspausdina console?
 var x = 444;
-fucntion test1(x) {
-  x=777;
-  window.x = x; // 444 = 777 globalus tampa local
-
-     x = 555;
+function test1(x) {
+      x = 777;
+      window.x = x;  // 444 = 777 globalus patampa locals
 }
 test1(333);
-console.log(x);
-
-var x = 999;
-function test1(x) {
-     console.log(x);
-     x = 555;
-}
-test1(888);
+console.log(x); // 777
 
 
 // let ir const  scope'ai viekia ir IF sakiniuose
--- dirbame_cia/diena_9/Tomas/6-pasiruošimas-scopeTestui/js/js-scopes-6-atvejai.js
+++ dirbame_cia/diena_9/Tomas/6-pasiruošimas-scopeTestui/js/js-scopes-6-atvejai.js
@@ -1,19 +1,19 @@
 
-// Ka atspausdina console?
+// 1. Ka atspausdina console?
 var x = 10;
 function test1() {
     console.log(x);
 }
-test1(); //10
+test1();
 
-// Ka atspausdina console?
+// 2. Ka atspausdina console?
 var x = 10;
 function test1() {
     console.log(x);
 }
-// nieko
 
-// Ka atspausdina console?
+
+// 3. Ka atspausdina console?
 var x = 5;
 function test1() {
     x = 0;
@@ -22,7 +22,7 @@ test1();
 console.log(x);
 
 
-// Ka atspausdina console?
+// 4. Ka atspausdina console?
 var x = 5;
 function test1() {
     x = 0;
@@ -30,8 +30,7 @@ function test1() {
 console.log(x);
 test1();
 
-
-// Ka atspausdina console?
+// 5. Ka atspausdina console?
 var x = 0;
 function test1() {
     var x = 100;
@@ -40,33 +39,33 @@ console.log(x);
 test1();
 
 
-// Ka atspausdina console?
+// 6. Ka atspausdina console?
 var x = 200;
 function test1(x) {
     var x = 100;
     console.log(x);
 }
-test1(555); //100
+test1(555);
 
-// Ka atspausdina console?
+// 7. Ka atspausdina console?
 var x = 50;
 function test1(x) {
     var x = 10;
 }
 test1(99);
-console.log(x); //50
+console.log(x);
 
 
-// Ka atspausdina console?
+// 8. Ka atspausdina console?
 var x = 22;
 function test1(x) {
      x = 11;
 }
 test1(13);
-console.log(x); //22
+console.log(x);
 
 
-// Ka atspausdina console?
+// 9. Ka atspausdina console?
 var x = 33;
 function test1(x) {
      x = 44;
@@ -75,41 +74,37 @@ function test1(x) {
 test1(55);
 
 
-// Ka atspausdina console?
+// 10. Ka atspausdina console?
 var x = 999;
 function test1(x) {
      console.log(x);
 }
 test1(888);
 
-// Ka atspausdina console?
+// 11. Ka atspausdina console?
 var x = 999;
 function test1(x) {
      console.log(x);
      x = 555;
 }
 test1(888);
-console.log( x );
 
-// Ka atspausdina console?
+// 12. Ka atspausdina console?
+var x = 999;
+function test1(x) {
+     x = 555;
+     console.log( this.x);
+}
+test1(888);
 
+// 13. Ka atspausdina console?
 var x = 444;
-fucntion test1(x) {
-  x=777;
-  window.x = x; // 444 = 777 globalus tampa local
-
-     x = 555;
+function test1(x) {
+      x = 777;
+      window.x = x;
 }
 test1(333);
 console.log(x);
-
-var x = 999;
-function test1(x) {
-     console.log(x);
-     x = 555;
-}
-test1(888);
-
 
 // let ir const  scope'ai viekia ir IF sakiniuose
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let
@@ -118,6 +113,6 @@ let x = 1;
 
 if (true) {
   let x = 2;
-  console.log(x); // 2
+  console.log(x); //
 }
-console.log(x);   //  1
+console.log(x);   //
