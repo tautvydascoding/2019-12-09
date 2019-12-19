055ac0c9be97a1a101578d90a7e43dee69072f1c
tautvydascoding <123@gmail.com>
2019-12-19 17:00:26 +0200

    klases darbas

--- uzduotys/diena_8/TEORIJA+++/js-scopes-6-atvejai.js
+++ uzduotys/diena_8/TEORIJA+++/js-scopes-6-atvejai.js
@@ -120,3 +120,17 @@ if (true) {
   console.log(x); // 2
 }
 console.log(x);   //  1
+
+//---------------------
+let x = 11;
+
+function test() {
+    let x = 22;
+    if (true) {
+        let x = 33;
+    }
+    console.log("viduje test()", x);
+}
+console.log("pries test()", x);
+test();
+console.log("po test()", x);
---
+++ uzduotys/komandinis+++/data/duomenys.js
@@ -0,0 +1,11 @@
+{
+"productID" : “KL2432”,
+“productName” : “Hugo Boss men’s suit”,
+“productPrice” :  2500,
+“productPriceFormatted” : “DKK 2.500,00”
+“inStock” : true,
+“relatedProducts”: [
+      {“productID”: “KL2532”},
+      {“productID”: “KL2341”}
+]
+}
