31fc876839ce65568bedc8f2cbd3158805e395b9
tautvydascoding <123@gmail.com>
2020-01-06 14:26:25 +0200

    klases darbai

--- uzduotys/diena_13/UZDUOTYS/1-uzduotis-sql.js
+++ uzduotys/diena_13/UZDUOTYS/1-uzduotis-sql.js
@@ -2,7 +2,7 @@
 // uzduotis 2:  Pakeisti Paciento "Co" varda i "Karolis"
 // uzduotis 3.1: visu pacientu, kuriu vardas yra "Tom" -  varda pakeisti i "Tomas"
 // uzduotis 3.2: visu pacientu, kuriu vardas prasideda "T" -  docotor_id pakeisti i "4"
-// uzduotis 4: gauti paciento "Mark" daktaro pavarde
+// uzduotis 4: gauti paciento "Mark" gydytojo pavarde
 
 
 
