c5786c4be25353db0d4f49c4bd81d90e9e9614b6
tautvydascoding <123@gmail.com>
2020-01-09 12:05:00 +0200

    klases darbas

--- uzduotys/diena_11/TEORIJA-/mysql-sql/idejos-db-lentelems.sql
+++ uzduotys/diena_11/TEORIJA-/mysql-sql/idejos-db-lentelems.sql
@@ -24,11 +24,11 @@ id    vardas   klausimas    tel_1              email
 =======vartotoju atsiliepimai/komentarai ========
 id    vardas   user_id   atsiliepimas/komentaras   ar_rodyti   data              email
 1     Tomas       13      Lorem....                true     2010-06-06          Tomas@tomas.lt
-2     Julius      64     Lorem....                 false    2010-06-05         julius@julius.lt
+2     Julius      64      Lorem....                 false    2010-06-05         julius@julius.lt
 
 ======= Prekes/ straisniai   ========
 id    pavadinimas   aprasymas      kaina    nuolaida  pozicija      data   kiekis
-1     Dviratis       Lorem....     659
+1     Dviratis        Lorem....     659
 2     Paspirtukas     Lorem....     49.99       5           99999
 3     Rieduciai       Lorem....     359         20          2
 
