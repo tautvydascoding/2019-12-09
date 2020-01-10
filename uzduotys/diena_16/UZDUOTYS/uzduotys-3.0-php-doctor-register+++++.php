975df2b1f0934bd338d9bfabf9a431f58259bf1b
tautvydascoding <123@gmail.com>
2020-01-10 11:15:34 +0200

    klases darbas

--- uzduotys/diena_16/UZDUOTYS/uzduotys-3.0-php-doctor-register+++++.php
+++ uzduotys/diena_16/UZDUOTYS/uzduotys-3.0-php-doctor-register+++++.php
@@ -5,14 +5,16 @@
 // 1.2. faile paleisti:
 include('db_functions.php');
 createDoctor("Juozas", "Juozaitis");
+echo "pacyko sukurti nauja gydytoja<br>";
 // 1.3. Narsykleje nueiti:
     localhost/ ... /naujasGydytojas.php
 
 
-deleteDoctor() paleidimas per narsykle
+
 // 2.1. sukurti PHP faila  trintiGydytoja.php
 // 2.2. faile paleisti:
 include('db_functions.php');
 deleteDoctor(4);
+echo "pavyko istrinti  gydytoja <br>";
 // 2.3. Narsykleje nueiti:
     localhost/ ... /trintiGydytoja.php
