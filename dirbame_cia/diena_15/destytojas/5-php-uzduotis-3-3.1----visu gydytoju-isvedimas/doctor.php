2896420ba86ac8c1d26541cf3b3a805aaa035d2d
tautvydascoding <123@gmail.com>
2020-01-10 10:00:49 +0200

    klases darbas

---
+++ dirbame_cia/diena_15/destytojas/5-php-uzduotis-3-3.1----visu gydytoju-isvedimas/doctor.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once('models/doctor-functions.php');
+
+$x = $_GET['numeris'];
+$gydytojas = getDoctor($x); // array - gydytrojo duomenis is DB
+
+// print_r($gydytojas); // test
+
+echo "<h2>Gydytojas</h2>";
+echo "<h3>  $gydytojas[name]   $gydytojas[lname]</h3>";
--- uzduotys/diena_15/UZDUOTYS++++/uzduotys-2.2-php-GET-savarankiska.php
+++ uzduotys/diena_15/UZDUOTYS++++/uzduotys-2.2-php-GET-savarankiska.php
@@ -4,19 +4,20 @@
 // 1. sukurti tuscia porjekta (default)
 
 // 2. doctor.php faile parasyti PHP koda:
-    $numeris = $_GET['nr'];
-    $gydytojas = getDoctors( $numeris);
+    $numeris = 2;
+    $gydytojas = getDoctor( $numeris);
     echo $gydytojas[1] .  $gydytojas[2] ;
+    // vienas is butu nesuveiks
     echo $gydytojas["name"] .  $gydytojas['lname'] ;
+
 
 // 3. pasileisti Narsykleje si puslapi (doctor.php ne index.php, Narsykles adreso ivedimo lange (paciam gale) prirasyti : doctor.php)
 // 3.1 istaisyti klaida: function 'getDoctors' not found
-// 3.1.1 isideti i projekta - faila: db_functions.php
+// 3.1.1 isideti i projekta faila: db_functions.php
+
 // 3.1.2  i  faila  doctor.php parasyti:
 include('db_functions.php');
-// 3.1.3 Narsykles adreso ivedimo lange (paciam gale) prirasyti :
-    doctor.php?nr=4
 
-// 4. Narsykles adrese  pakeisti ?nr=3
-// 4. Narsykles adrese  pakeisti ?nr=2 
+// 4. faile  pakeisti $numeris = 3;
 // ir paziureti kas bus
+// 4. faile  pakeisti $numeris = 4;
