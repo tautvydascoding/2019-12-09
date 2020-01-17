de7d76a820f525d5d46274eb68bc0daf3f54eea7
giedriusbalynas <giedriusbalynas@users.noreply.github.com>
2020-01-17 09:53:15 +0200

    20 dienos layout testas

---
+++ dirbame_cia/diena_20/GiedriusB/layout-testas/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="css/normalize.css">
+
+        <link rel="stylesheet" href="libs/bootstrap/css/bootstrap.min.css">
+
+        <!-- MANO CSS visada pats zemiausias!!! -->
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div class="container  bg-light">
+            <div class="row aukstis-100 bg-dark m-1">
+              <div class="col text-light">headerio textas</div>
+            </div>
+            <div class="row m-1 bg-dark">
+              <div class="col text-light">siaura eilute</div>
+            </div>
+            <div class="row m-1">
+              <div class="col-8 text-light">
+                <div class="row bg-dark aukstis-200 mb-1">
+                  <div class="col">didelis staciakampis 1</div>
+                </div>
+                <div class="row bg-dark aukstis-200 mb-1">
+                  <div class="col">didelis staciakampis 1</div>
+                </div>
+                <div class="row bg-dark aukstis-200 mb-1">
+                  <div class="col">didelis staciakampis 1</div>
+                </div>
+              </div>
+              <div class="col">
+                <div class="row">
+                  <div class="col bg-dark text-light ml-1 aukstis-100 mb-1">mazesnis staciakampis</div>
+                </div>
+                <div class="row">
+                  <div class="col bg-dark text-light ml-1 aukstis-200 mb-1">mazesnis staciakampis</div>
+                </div>
+                <div class="row">
+                  <div class="col bg-dark text-light ml-1 aukstis-200 mb-1">mazesnis staciakampis</div>
+                </div>
+                <div class="row">
+                  <div class="col">
+                    <div class="row">
+                      <div class="col-6 bg-dark text-light ml-1">
+                        Sunkesne 1
+                      </div>
+                      <div class="col bg-dark text-light ml-1 aukstis-50">
+                        Sunkesne 2
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col bg-dark text-light ml-1 mt-1 aukstis-50">
+                        Sunkesne per abu
+                      </div>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row m-1">
+              <div class="col bg-dark text-light">
+                Plonytis footeris
+
+              </div>
+
+            </div>
+
+
+
+
+        </div>
+
+
+
+
+        <script type="text/javascript" src="libs/jquery-3.4.1.min.js"> </script>
+        <script type="text/javascript" src="libs/bootstrap/js/bootstrap.bundle.min.js"> </script>
+        <!-- MANO JS pats zemiausias -->
+        <script type="text/javascript" src="js/main.js"></script>
+
+     </body>
+</html>
